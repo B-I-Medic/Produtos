52db73f7b330400a3bfac3d77e6cf8cb21bdbd60
Yuri Medeiros <bi2@medic.com.br>
2026-07-16 17:02:56 -0300

    alter tests
    Signed-off-by: Yuri Medeiros <bi2@medic.com.br>

--- app/web/src/main/java/com/medic/Web/repository/forecast/ForecastRepositoryCustom.java
+++ app/web/src/main/java/com/medic/Web/repository/forecast/ForecastRepositoryCustom.java
@@ -2,10 +2,8 @@ package com.medic.Web.repository.forecast;
 
 import com.medic.Web.dto.forecast.ForecastFilterDTO;
 import com.medic.Web.dto.forecast.ForecastAgrupadoResponseDTO;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
-@Repository
 public interface ForecastRepositoryCustom {
 
     Flux<ForecastAgrupadoResponseDTO> findByFilter(ForecastFilterDTO filter);
